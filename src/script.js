9b8c4f11b128cce70c3e663e301922c8c8d0155d
Sudeepto Bose <sudeeptobose23@gmail.com>
2022-02-26 10:12:49 +0600

    added debug-gui

--- src/script.js
+++ src/script.js
@@ -1,6 +1,10 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'lil-gui'
+
+// Debug
+const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -12,8 +16,20 @@ const scene = new THREE.Scene()
 const material = new THREE.MeshBasicMaterial()
 const geometry = new THREE.BoxGeometry()
 const mesh = new THREE.Mesh( geometry, material)
+
+const parameters = {
+    color: 0xffffff
+}
+
 scene.add(mesh)
 
+gui.add(mesh.position, 'x').min(- 3).max(3).step(0.01).name('Horizontal')
+gui.add(mesh.position, 'y').min(- 3).max(3).step(0.01).name('Vertical')
+gui.add(mesh, 'visible').name('Visibility')
+gui.add(material, 'wireframe').name('Wireframe')
+gui.addColor(parameters, 'color').onChange(()=>{
+    material.color.set(parameters.color)
+})
 // Sizes
 const sizes = {
     width: window.innerWidth,
@@ -58,6 +74,35 @@ controls.enableDamping = true
 // Clock
 
 const clock = new THREE.Clock()
+
+// Fullscreen
+window.addEventListener('dblclick', () =>
+{
+    const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement
+
+    if(!fullscreenElement)
+    {
+        if(canvas.requestFullscreen)
+        {
+            canvas.requestFullscreen()
+        }
+        else if(canvas.webkitRequestFullscreen)
+        {
+            canvas.webkitRequestFullscreen()
+        }
+    }
+    else
+    {
+        if(document.exitFullscreen)
+        {
+            document.exitFullscreen()
+        }
+        else if(document.webkitExitFullscreen)
+        {
+            document.webkitExitFullscreen()
+        }
+    }
+})
 
 // Tick function
 const tick = ()=>
